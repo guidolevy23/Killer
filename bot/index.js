c7e5c7bdc927db5f309ef8901689ffdaad077a79
Guido <levyguidocarp@gmail.com>
2023-05-13 11:36:24 -0300

    respuesta con foto

--- bot/index.js
+++ bot/index.js
@@ -3,6 +3,39 @@ const axios = require('axios')
 const MESSAGES = require('./messages.js')
 const FUNCTIONS = require('./functions.js')
 const bot = new Telegraf('5689007865:AAEvGL1IXtp-c12ws-P_Y9cnUTnLlnTthcA');
+// const cloudinary = require('cloudinary').v2;
+
+// // Configuration 
+// cloudinary.config({
+//     cloud_name: "di9meg3xu",
+//     api_key: "346486911846515",
+//     api_secret: "zBVk8tO79UcA4cSUY7h2pa-MpCU"
+//   });
+  
+  
+//   // Upload
+  
+//   const res = cloudinary.uploader.upload('./ladescon.jpeg', {public_id: "la_descon"})
+  
+//   res.then((data) => {
+//     console.log(data);
+//     console.log(data.secure_url);
+//   }).catch((err) => {
+//     console.log(err);
+//   });
+  
+  
+//   // Generate 
+//   const url = cloudinary.url("la_descon", {
+//     width: 100,
+//     height: 150,
+//     Crop: 'fill'
+//   });
+  
+  
+  
+//   // The output url
+//   console.log(url);
 
 
 bot.start(async(ctx)=>{
@@ -51,6 +84,16 @@ bot.command('reiniciar', async ctx=>{
     FUNCTIONS.deleteGame(ctx)
     ctx.reply(MESSAGES.replay)
 })
-    
-    
-    bot.launch()
+
+bot.hears(/la descon/,async ctx=>{
+    try{
+        const image = "https://res.cloudinary.com/di9meg3xu/image/upload/v1683772387/la_descon.jpg"
+        await ctx.replyWithPhoto(image);
+        await ctx.reply("AGUANTE LA DESCON WACHO")
+
+    }catch(error){
+        console.log(error)
+    }
+})
+
+bot.launch()
